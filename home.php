b3d527ebb863aa9c0d692eddcdfd9d62a70a1bec
yashvirathaud642 <145859050+yashvirathaud642@users.noreply.github.com>
2023-12-26 15:07:26 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,24 @@
+<!--<p>welcome user</p>-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
 <?php
-include 'dp.php';
-//echo "Welcome User";
+        include 'loginNavbar.php';
+        require 'dp.php';
+        ?>
+        <div class="container">
+            <h1 class="mt-3 text-center">Users of Student CRUD System.</h1>
+        </div>
+    
+</body>
+</html>
+<?php
+//include 'db.php';
+ //echo "<script>alert('welcome user');</script> ";
 ?>
