2676955cc42f0642d79d3ac6da45f9004e093f17
yashvirathaud642 <145859050+yashvirathaud642@users.noreply.github.com>
2023-12-28 12:00:33 +0530

    Add files via upload

---
+++ update.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update data</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+<?php
+    require "dp.php";
+    include 'loginNavbar2.php';
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+    }
+    
+    ?>
+    <div class="container">
+        <h1 class="mt-3">Update Record</h1>
+   
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data" id="createform">
+        <div class="form-group">
+            <p class="text-secondary">Please fill this form and submit to update admission record to database.</p>
+            <input type="hidden" name="id" id="id" class="form-control mt-2">
+        </div>
+        <div class="form-group">
+                    <label for="name1" class="mt-2">Name</label>
+                    <input type="text" name="name" id="name" class="form-control mt-2" required>
+                </div>
+                <div class="form-group">
+                    <label for="country" class="mt-2">Country</label>
+                    <!--<input type="text" name="country" id="country" class="form-control mt-2"required>-->
+                            <select class="form-control mt-2" id="country" onchange="updateForm()">
+                                <option value="country">select country</option>
+                                <option value="country">Madhya pradesh</option>
+                            </select>
+                </div>
+                <div class="form-group">
+                    <label for="state" class="mt-2">State</label>
+                   <!-- <input type="text" name="state" id="state" class="form-control mt-2"required>-->
+                   <select class="form-control mt-2" id="state" onchange="updateForm()">
+                                <option value="state">select state</option>
+                                <option value="state">Gujrat</option>
+                            </select>
+                </div>
+                <div class="form-group">
+                    <label for="city" class="mt-2">City</label>
+                   <!-- <input type="text" name="city" id="city" class="form-control mt-2"required>-->
+                   <select class="form-control mt-2" id="city" onchange="updateForm()">
+                                <option value="city">select city</option>
+                                <option value="city">India</option>
+                            </select>
+                </div>
+                <div class="form-group">
+                <label for="userimage" class="mt-2">User Image</label>
+                <input class="form-control mt-2" type="file" name="userimage" id="userimage">
+            </div>
+            <div class="mt-3 mb-3">
+                    <button type="submit" class="btn btn-outline-success" id="btnadd">Submit</button>
+                    <a href="cancel.php" class="btn btn-outline-danger ml-2">Cancel</a>
+                </div>
+    </form>
+   
+    </div>
+</body>
+</html> 
+
+
+
+
+
---
+++ view.php
@@ -0,0 +1,89 @@
+<?php
+$id = $name = $class= $section = $country = $state = $city = $image='' ;
+$id = $_GET['id'];
+include 'dp.php';
+$sql = "select * from users where id='$id';";
+$res = mysqli_query($con, $sql);
+$data = mysqli_fetch_array($res);
+
+$name = $data['name'];
+$country = 'null';
+$state = 'null';
+$city = 'null';
+$image = 'userimage';
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Read</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+    <?php
+    //require "dp.php";
+    //echo "welcome user";
+    ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+   <nav class="navbar navbar-expand-lg navbar-light bg-dark p-3">
+    <a class="navbar-brand text-info text-white" href="login.php">Student Managment System </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="login nav-link text-info text-white" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="register nav-link text-info text-white" href="manage.php">Manage User</a>
+        </li>
+      </ul>
+    </div>
+    </div>
+    <div class="topnav-right">
+    <a  class="text-info text-white" href="#" style="text-decoration: none;"><i class="fa-solid fa-user"></i>Profile</a>&nsbp;
+    <a class="text-info text-white" href="#"  style="text-decoration: none;"><i class="fa-solid fa-right-from-bracket"></i>Logout</a>
+  </div>
+  </nav>
+  <div class="container p-3">
+            <h1 class="mt-3">Details</h1>
+           
+          <table class="table table-bordered table-stripped">
+          <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Name</td>
+                            <td>Country</td>
+                            <td>State</td>
+                            <td>City</td>
+                            <td>Image</td>
+                           
+                        </tr>    
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $id;?></td>
+                            <td><?php echo $name;?></td>
+                            <td><?php echo $country;?></td>
+                            <td><?php echo $state;?></td>
+                            <td><?php echo $city;?></td>
+                            <td><img src="<?php echo $image?>"></td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+                </div>
+                
+
+         
+    
+</body>
+</html>
