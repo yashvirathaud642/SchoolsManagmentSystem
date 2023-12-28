b306757c936379cabded40c3bfc58cf07447b0a2
yashvirathaud642 <145859050+yashvirathaud642@users.noreply.github.com>
2023-12-28 12:01:35 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,23 +1,22 @@
 <!--<p>welcome user</p>-->
-<?php
-include 'dp.php';
- //echo "<script>alert('welcome user');</script> ";
-?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
 <?php
         //include 'loginNavbar.php';
-        //require 'db.php';
+        require 'dp.php';
         ?>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <nav class="navbar navbar-expand-lg navbar-light bg-dark p-3">
-    <a class="navbar-brand text-info" href="login.php">Student Managment System </a>
+    <a class="navbar-brand text-info text-white" href="login.php">Student Managment System </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -26,72 +25,65 @@ include 'dp.php';
 
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="login nav-link text-info" href="home.php">Home</a>
+          <a class="login nav-link text-info text-white" href="home.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="register nav-link text-info" href="manage.php">Manage User</a>
+          <a class="register nav-link text-info text-white" href="manage.php">Manage User</a>
         </li>
       </ul>
     </div>
+    </div>
+    <div class="topnav-right">
+    <a  class="text-info text-white" href="#" style="text-decoration: none;"><i class="fa-solid fa-user"></i>Profile</a>&nsbp;
+    <a class="text-info text-white" href="#"  style="text-decoration: none;"><i class="fa-solid fa-right-from-bracket"></i>Logout</a>
+  </div>
   </nav>
         <div class="container">
             <h1 class="mt-3 text-center">Users of Student CRUD System.</h1>
+          </div> 
+          <br><br>
+        <div class="container">
+            <?php
+                $sql = "SELECT * from users ORDER by id ASC;";
+                $res = mysqli_query($con,$sql);
+                if($res){
+                    if(mysqli_num_rows($res)>0){
+                        echo '<table class="table table-bordered table-striped">';
+                        echo "<thead>";
+                            echo "<tr>";
+                                echo "<th>ID</th>";
+                                echo "<th>Email</th>";
+                                echo "<th>Role</th>";
+                            echo "</tr>";
+                        echo "</thead>";
+                        echo "<tbody>";
+                        while($row = mysqli_fetch_array($res)){
+                            echo "<tr>";
+						        echo "<td>" . $row['id'] . "</td>";
+						        echo "<td>" . $row['email'] . "</td>";
+						        echo "<td>" . 'user' . "</td>";
+						    echo "<tr>";
+                        }
+                    echo "</tbody>";                            
+                    echo "</table>";
+                    mysqli_free_result($res);
+                    }
+                    else{
+                        echo '<div class="alert alert-danger">
+                                No Record were Found.
+                            </div>';
+                    }
+                }
+                else{
+                    echo 'OOPs! something went wrong. please try again later.';
+                 }
+            mysqli_close($con);
+            ?>
         </div>
-    <div class="container">
-    <table class="table table-striped">
-  <thead class="thead-light">
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Role</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Yashvi Rathaud</td>
-      <td>yashvirathore604@gmail.com</td>
-      <td>user</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Aishrasha Soni</td>
-      <td>aishrashasoni@gmail.com</td>
-      <td>user</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Aayush Patidar</td>
-      <td>aayushpatidar04@gmail.com</td>
-      <td>user</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Prtik Patidar</td>
-      <td>pratikpatidar@gmail.com</td>
-      <td>user</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Nikhil Sharma</td>
-      <td>Nikhil@gmail.com</td>
-      <td>user</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Shivani Sharma</td>
-      <td>shivani@gmail.com</td>
-      <td>user</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Anshuman Sharma</td>
-      <td>anshuman@gmail.com</td>
-      <td>user</td>
-    </tr>
-  </tbody>
-</table>
-    </div>
+       
 </body>
 </html>
+<?php
+//include 'db.php';
+ //echo "<script>alert('welcome user');</script> ";
+?>
